232ef468b2253ca99ffb5c12c418a2d8f1d467fc
avinashmishra <avinashm720@gmail.com>
2023-07-09 07:32:05 +0530

    Add MoviesApp in MainActivity

--- app/src/main/java/com/deepshooter/moviesapp/MainActivity.kt
+++ app/src/main/java/com/deepshooter/moviesapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.deepshooter.moviesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.deepshooter.moviesapp.ui.MoviesApp
 import com.deepshooter.moviesapp.ui.theme.MoviesAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesAppTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MoviesApp()
+                }
             }
         }
     }
